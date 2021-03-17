8cd69450a06d0ace7fda98fbdb957e1ea1c291c5
AlyreMix <494261741@qq.com>
2021-03-17 12:57:31 +0800

    add header navbar

---
+++ src/components/Layout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+  <div class="header">这是顶栏</div>
+  <div class="navbar">这是左侧栏</div>
+  <div class="main">主栏</div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Layout"
+}
+</script>
+
+<style scoped>
+.header {
+  position: absolute;
+  line-height: 70px;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  background-color: rgba(255, 0, 0, 0.3);
+}
+
+.navbar {
+  position: absolute;
+  width: 320px;
+  top: 70px;
+  left: 0px;
+  bottom: 0px;
+  background-color: rgba(0, 255, 0, 0.3);
+}
+
+.main {
+  position: absolute;
+  top: 70px;
+  left: 320px;
+  bottom: 0px;
+  right: 0px;
+  padding: 10px;
+  background-color: rgba(0, 0, 255, 0.3);
+}
+</style>
